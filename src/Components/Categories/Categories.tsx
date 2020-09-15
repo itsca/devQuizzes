0ceda5cf887e81861a533fa86ceea42824720669
Itsca <itscae@gmail.com>
2020-09-15 16:16:59 -0600

    chore: adds category rendering and quizz sorting

--- src/Components/Categories/Categories.tsx
+++ src/Components/Categories/Categories.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Grid } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Card, Grid, Typography } from '@material-ui/core';
 import { QuizInterface } from '../Quiz/Quiz'
 
 interface Props {
@@ -8,10 +8,36 @@ interface Props {
   }
 }
 
+/**
+ * Extracts the categories from the quizzes and makes sure there only one of each.
+ * @param quizzes
+ */
+const extractCategories = (quizzes: Props['quizzes']): string [] => {
+  let extractedCategories: string[] = []
+  for (let [key, value] of Object.entries(quizzes)) {
+    if (extractedCategories.indexOf(value.category) === -1) extractedCategories.push(value.category);
+  }
+  return extractedCategories
+}
+
 const Categories: React.FC<Props> = (props: Props) => {
+  const [extractedCategories, setExtractedCategories] = useState(extractCategories(props.quizzes))
   return (
     <Grid className="Categories">
-      Categories
+      {extractedCategories.map((category, i) => <Grid container key={i}>
+        <Typography variant="h2">
+          {category}
+        </Typography>
+        {
+          Object.keys(props.quizzes)
+            .filter((k, i) => props.quizzes[k].category === category)
+            .map(k => <Card>
+              <Typography>
+                {props.quizzes[k].name}
+              </Typography>
+            </Card>)
+        }
+      </Grid>)}
     </Grid>
   );
 }
--- src/Data/combinedQuizzes.ts
+++ src/Data/combinedQuizzes.ts
@@ -1,3 +1,4 @@
 import react from './quizzes/react.json'
 import angular from './quizzes/angular.json'
-export default { react, angular };
+import python from './quizzes/python.json'
+export default { react, angular, python };
